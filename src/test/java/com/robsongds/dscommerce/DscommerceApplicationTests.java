544fa7484cd7d974aa57de7545010155837869f9
Robson <robson.080490@gmail.com>
2024-07-23 12:08:22 -0300

    first commit

---
+++ src/test/java/com/robsongds/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robsongds.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
